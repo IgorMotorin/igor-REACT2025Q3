951ae0bdba307284504ada0eebb304c1c49e2422
Igor <igor.motorin@gmail.com>
2025-07-20 11:57:01 +0300

    feat: add interaction  test for Search

--- src/__tests__/component/Search.test.tsx
+++ src/__tests__/component/Search.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import Search from '../../component/Search';
 import App from '../../App';
 
@@ -75,8 +77,47 @@ describe('component Search Renders', () => {
 });
 
 describe('User Interaction Tests', () => {
-  it('should Updates input value when user types', () => {});
-  it('Saves search term to localStorage when search button is clicked', () => {});
+  it('should Updates input value when user types', async () => {
+    render(<App />);
+
+    const input = screen.getByRole('searchbox');
+    const user = userEvent.setup();
+    await user.click(input);
+    await user.keyboard('userinput');
+
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue('userinput');
+  });
+  it('Saves search term to localStorage when search button is clicked', async () => {
+    const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+    const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
+
+    beforeEach(() => {
+      // Start each test with a clean slate
+      localStorage.clear();
+      getItemSpy.mockClear();
+      setItemSpy.mockClear();
+    });
+
+    render(<App />);
+
+    const input = screen.getByRole('searchbox');
+    const button = screen.getAllByRole('button');
+    const user = userEvent.setup();
+    await user.click(input);
+    await user.keyboard('userinputtext');
+    await user.click(button[0]);
+
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue('userinputtext');
+    expect(button[0]).toBeInTheDocument();
+
+    expect(setItemSpy).toHaveBeenCalledWith('appkey', 'userinputtext');
+
+    getItemSpy.mockReturnValue('userinputtext');
+    const ls = localStorage.getItem('appkey');
+    expect(ls).toBe('userinputtext');
+  });
   it('Trims whitespace from search input before saving', () => {});
   it('Triggers search callback with correct parameters', () => {});
 });

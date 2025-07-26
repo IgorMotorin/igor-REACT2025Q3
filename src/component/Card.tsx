48f35af0956906bd45c4f3644f9751b64c261576
Igor <igor.motorin@gmail.com>
2025-07-26 19:02:23 +0300

    refactor: Card

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,20 +1,16 @@
-import { Component } from 'react';
-
-class Card extends Component<{ name: string; text: string }> {
-  static readonly defaultProps = {
-    name: '',
-    text: '',
-  };
-  render() {
-    return (
-      <li className="relative m-1 w-60 rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
-        <div className="bg-white p-5 rounded-md">
-          <h1 className="font-bold text-xl mb-2">{this.props.name}</h1>
-          <p>{this.props.text}</p>
-        </div>
-      </li>
-    );
-  }
+export default function Card({
+  name = '',
+  text = '',
+}: Readonly<{
+  name: string;
+  text: string;
+}>) {
+  return (
+    <li className="relative m-1 w-60 rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
+      <div className="bg-white p-5 rounded-md">
+        <h1 className="font-bold text-xl mb-2">{name}</h1>
+        <p>{text}</p>
+      </div>
+    </li>
+  );
 }
-
-export default Card;

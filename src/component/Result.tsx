0d88bbccb276684deab8e69ce2ee6cff34c2e6a6
Igor <igor.motorin@gmail.com>
2025-07-26 19:20:22 +0300

    refactor: Result

--- src/component/Result.tsx
+++ src/component/Result.tsx
@@ -1,25 +1,20 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import Card from './Card';
 
-class Result extends Component<{ cards: { fact: string; text: string }[] }> {
-  static readonly defaultProps = {
-    cards: [{ fact: '', text: '' }],
-  };
-  render() {
-    return (
-      <ul className="flex justify-center flex-wrap">
-        {this.props.cards.length > 0 ? (
-          this.props.cards.map((itm, idx) => {
-            return (
-              <Card key={idx + 'a'} name={itm.fact} text={itm.text}></Card>
-            );
-          })
-        ) : (
-          <Card key={'a1'} name={'no result'} text={'no result'}></Card>
-        )}
-      </ul>
-    );
-  }
+export default function Result({
+  cards,
+}: Readonly<{
+  cards: { fact: string; text: string }[];
+}>) {
+  return (
+    <ul className="flex justify-center flex-wrap">
+      {cards.length > 0 ? (
+        cards.map((itm, idx) => {
+          return <Card key={idx + 'a'} name={itm.fact} text={itm.text}></Card>;
+        })
+      ) : (
+        <Card key={'a1'} name={'no result'} text={'no result'}></Card>
+      )}
+    </ul>
+  );
 }
-
-export default Result;
